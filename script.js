9c723cba39848af4328cd2f69e8c735569bbd530
Cori Beecroft <CoriBeecroft@users.noreply.github.com>
2017-09-02 19:25:31 -0700

    Implemented the first part of story selection
    There are a lot of other changes because I wasn't sure how to pass information from child components to parent components. I spent a lot of time trying to figure out the React way to do this so were lots of other minor change that I made while trying to figure this out. I'm leaving the lifecycle functions in because I found it very useful to see when they are being called. There is still more to do with the story selection, but this code is really messy and hard to understand so I think I need to do some serious clean-up before proceeding.

--- script.js
+++ script.js
@@ -10,31 +10,86 @@ class HackerNews extends React.Component
 		this.state = 
 		{
 			currentStory: -1, 
-			currentStoryUrl: "",
 		}
 	  	
 		this.handleClick = (e, f) => 
 		{
-    		var id = $(e.target).data('id');
-    		var url = $(e.target).data('url');
+			var stories = this.props.stories;
+			var newCurrent = this.state.currentStory; 
+			for(var i=0; i<stories.length; i++)
+			{
+				if(stories[i].active && this.state.currentStory !== i)
+				{
+					newCurrent = i;
+				}
+				else if(stories.active)
+				{
+					stories.active = false;
+				}
+			}
+
+			this.setState(() => {
+    			return {
+    				currentStory: newCurrent
+    			}
+    		});
 		};
+	}
+
+
+
+	shouldComponentUpdate()
+	{
+		//console.log("shouldComponentUpdate");
+		return true;
+	}
+
+	componentWillUpdate(nextProps)
+	{
+		//console.log("componentWillUpdate");
+
+	}
+
+	componentDidMount()
+	{
+		setInterval(()=>
+		{
+			// console.log("Element 0 is active: " + this.props.stories[0].active);
+			// console.log("State.currentStory: " + this.state.currentStory);
+
+		}, 1000);
 	}
 
 	render()
 	{
+		//console.log("render");
 		return (<div>
 			<Header />
 			<main>
 				<div onClick={this.handleClick}>{this.props.stories.map((model, index) => 
 					{
-						return <StoryInfo model={model} key={index} index={index} />;	
-						
+						if(this.state.currentStory == index)
+						{
+							model.active = true;
+							return <StoryInfo model={model} key={index} index={index} />;
+						}
+						else
+						{
+							model.active = false;
+							return <StoryInfo model={model} key={index} index={index} />;
+						}
 					})}
 				</div>
+
 				{<StoryContent model={this.props.stories[0]}/>}
 			</main>
 			<Footer />
 		</div>);
+	}
+
+	componentDidUpdate(nextProps)
+	{
+		//console.log("componentDidUpdate");
 	}
 }
 
@@ -45,30 +100,27 @@ class StoryInfo extends React.Component
   		super();
 
   		this.props = props;
+  		this.props.model.active = props.active;
   		this.model = props.model;
-  	}
 
-  	handleViewAskClick()
-  	{
-  		console.log("Oh no! You have not yet implemented this button's click handler, now it will explode in 10... 9... 8...");
+  		this.handleViewAskClick = () =>
+	  	{
+	  		this.props.model.active = true;
+	  	}
   	}
 
 	render() 
 	{
-		var classNames = "story-info " + (this.props.active ? "active" : "");
+		var classNames = "story-info " + (this.props.model.active ? "active" : "");
 		var commentsURL = "https://news.ycombinator.com/item?id=" + this.model.id;
  
-		return (
-			<div className="story" id={this.props.index}>
-				<div className={classNames} data-id={this.model.id} data-url={this.model.url}>
+		return (<div className={classNames} id={this.props.index} data-url={this.model.url} onClick={this.handleViewAskClick}>
 					<h3>{this.model.title}</h3>
 					<span>{this.model.by}  |  {new Date(this.model.time).toString()}  |  {this.model.score}<br /></span>
 					{this.model.url && <button><a href={this.model.url} target="_blank">View Story</a></button>}
-					{this.model.text && <button onClick={this.handleViewAskClick}>View Story</button>}
+					{this.model.text && <button>View Story</button>}
 					<button><a href={commentsURL} target="_blank">Comments<br /></a></button>
-				</div>
-			</div>
-		);
+				</div>);
 	}
 }
 
@@ -77,45 +129,6 @@ class StoryContent extends React.Component
 
 	render()
 	{
-		this.props.model.text = `Ik bun Vulgaris Magistralis
-En ik ri-j op een mammoet in het rond
-
-Ik kok mien potjen op een werkende vulkaan
-Een dinosauris nuum ik een halve haan
-Wodan en Donar bunt achterneef'n van mien
-Moar die heb ik al eeuwen niet gezien
-
-Ik leaf alleen in de nacht
-In 't donker op jacht
-
-Ik bun Vulgaris Magistralis
-En ik ri-j op een mammoet in 't rond
-Ik bun Vulgaris Magistralis
-En op zondag op een mastodon't
-
-Een maffe professor die had van mien geheurd
-En kwam op zien Solex noar de Achterhoek gescheurd
-Met camera's en lasso's maakt ze jacht op mien
-Moar mien hol is nooit ontdekt en gin mens hef mien gezien
-
-Ik bun een woar kampioen
-In een echt visioen
-
-Ik bun Vulgaris Magistralis
-En ik ri-j op een mammoet in 't rond
-Ik bun Vulgaris Magistralis
-En op zondag op een mastodon't
-
-Ik sluup deur de bossen van de Achterhoek
-Zie wilt mien strikken veur 't witte doek
-Bi-j nacht en ontij kom ik uut mien hol
-Mies Bouwman en Spielberg wilt mien in de hoofdrol
-
-Moar zie kriegt mien niet
-Nee, zie kriegt mien niet
-Nee, zie kriegt mien nooit, nooit, nooit!
-`;
-
 
 		return (<div className="story-content">
 			<p>{this.props.model.text}</p>
