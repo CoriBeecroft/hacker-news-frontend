4caa2626e0f723430a2b6cce67df23fc8673877c
Cori <coribeecroft@gmail.com>
2023-04-06 13:02:21 -0700

    Made it so that fish come in from the left and the right.

--- src/HackerNewsFish.js
+++ src/HackerNewsFish.js
@@ -7,6 +7,9 @@ import "./HackerNewsFish.scss";
 function getRandomInt(lowerBoundInclusive, upperBound) {
     return Math.floor(Math.random() * upperBound + lowerBoundInclusive)
 }
+function getRandomSign() {
+    return getRandomInt(0, 2) === 0 ? -1 : 1
+}
 
 export function HackerNews() {
     const [ storyIds, setStoryIds ] = useState([]);
@@ -27,7 +30,7 @@ export function HackerNews() {
 
     useEffect(() => {
         if(storyIds.length == 0) { return; }
-        Promise.all(storyIds.slice(0, 10).map(id =>
+        Promise.all(storyIds.slice(0, 150).map(id =>
 			fetch(HN_API_URL + "/item/" + id + ".json")
 				.then(response => response.json())
 		)).then(stories => {
@@ -42,21 +45,32 @@ export function HackerNews() {
     function generateTargetYPosition(fishElement, initialYPosition) {
         return Math.min(
             Math.max(
-                (initialYPosition + (getRandomInt(0, 2) === 0 ? -1 : 1)*(getRandomInt(0, 75) + 1)), 0
+                (initialYPosition + getRandomSign()*(getRandomInt(0, 75) + 1)), 0
             ),
             window.innerHeight - fishElement.getBoundingClientRect().height
         )
     }
+
+    function targetYPositionReached(f, newYPosition) {
+        return (f.yDirection < 0 && newYPosition < f.targetYPosition) ||
+            (f.yDirection > 0 && newYPosition > f.targetYPosition)
+    }
+    function targetXPositionReached(f) {
+        const xPosition = f.initialXPosition + f.xDirection*f.xSpeed*(prevTimeRef.current - f.xStartTime);
+        return (f.xDirection < 0 && xPosition < f.targetXPosition) ||
+            (f.xDirection > 0 && xPosition > f.targetXPosition)
+    }
+
     function initializeFish(f, time) {
         const fishElement = f.ref.current;
-        const initialXPosition = window.innerWidth;
+        const initialXPosition = f.xDirection < 0 ? window.innerWidth : -1*fishElement.getBoundingClientRect().width;
         const initialYPosition = getRandomInt(0, (window.innerHeight - fishElement.getBoundingClientRect().height))
         fishElement.style.transform = `translate(${ initialXPosition }px, ${ initialYPosition }px)`
         const targetYPosition = generateTargetYPosition(fishElement, initialYPosition)
         const yDirection = (targetYPosition - initialYPosition)/Math.abs(targetYPosition - initialYPosition)
         setFish(oldFish => oldFish.map(of => of.id === f.id ? {
                 ...f,
-                targetXPosition: 0,
+                targetXPosition: f.xDirection < 0 ? -1*fishElement.getBoundingClientRect().width : window.innerWidth,
                 targetYPosition,
                 xStartTime: time,
                 yStartTime: time,
@@ -67,40 +81,44 @@ export function HackerNews() {
         )
     }
 
+    function updateFishPosition(f, time) {
+        const newXPosition = f.initialXPosition + f.xDirection*f.xSpeed*(time - f.xStartTime)
+        const newYPosition = f.initialYPosition + f.yDirection*f.ySpeed*(time - f.yStartTime)
+        f.ref.current.style.transform = `translate(${ newXPosition }px, ${ newYPosition }px)`
+
+        if(targetXPositionReached(f)) {
+            setFish(oldFish => oldFish.filter(of => f.id !== of.id))
+        } else if(targetYPositionReached(f, newYPosition)) {
+            setFish(oldFish => oldFish.map(of => {
+                if(of.id !== f.id) { return of; }
+                const initialYPosition = newYPosition;
+                const targetYPosition = generateTargetYPosition(f.ref.current, initialYPosition);
+                const yDirection = (targetYPosition - initialYPosition)/Math.abs(targetYPosition - initialYPosition)
+                return {
+                    ...of,
+                    initialYPosition,
+                    targetYPosition,
+                    yDirection,
+                    yStartTime: time
+                }
+            }))
+        }
+    }
+
     const frame = time => {
         if(time != prevTimeRef.current) {
             fish.forEach(f => {
                 if(f.ref && f.ref.current && !f.xStartTime) {
                     initializeFish(f, time);
                 } else if(f.ref && f.ref.current) {
-                    const newXPosition = f.initialXPosition + f.xDirection*f.xSpeed*(time - f.xStartTime)
-                    const newYPosition = f.initialYPosition + f.yDirection*f.ySpeed*(time - f.yStartTime)
-                    f.ref.current.style.transform = `translate(${ newXPosition }px, ${ newYPosition }px)`
-                    if(newXPosition < f.targetXPosition) {
-                        // all done, remove fish from DOM
-                    }
-                    if((f.yDirection < 0 && newYPosition < f.targetYPosition) ||
-                            (f.yDirection > 0 && newYPosition > f.targetYPosition)) {
-                        setFish(oldFish => oldFish.map(of => {
-                            if(of.id !== f.id) { return of; }
-                            const initialYPosition = newYPosition;
-                            const targetYPosition = generateTargetYPosition(f.ref.current, initialYPosition);
-                            const yDirection = (targetYPosition - initialYPosition)/Math.abs(targetYPosition - initialYPosition)
-                            return {
-                                ...of,
-                                initialYPosition,
-                                targetYPosition,
-                                yDirection,
-                                yStartTime: time
-                            }
-                        }))
-                    }
+                    updateFishPosition(f, time);
                 }
             })
         }
         prevTimeRef.current = time;
         animationFrameRef.current = requestAnimationFrame(frame);
     };
+
     useEffect(() => {
         if(animationFrameRef.current) {
             cancelAnimationFrame(animationFrameRef.current);
@@ -116,7 +134,7 @@ export function HackerNews() {
                 const fishToAdd = generateFish(storyQueue.current.shift())
                 setFish(oldFish => oldFish.concat([ fishToAdd ]))
             }
-        }, 4000)
+        }, 6000)
         return () => clearInterval(interval)
     }, [])
 
@@ -124,8 +142,8 @@ export function HackerNews() {
         return {
             id: storyInfo.id,   // TODO: consider making this a different id
             targetXPosition: null, targetYPosition: null,
-            xDirection: -1, xSpeed: 0.07,
-            yDirection: 1, ySpeed: 0.03,
+            xDirection: getRandomSign(), xSpeed: 0.03,
+            yDirection: getRandomSign(), ySpeed: 0.03,
             storyInfo,
         }
     }
