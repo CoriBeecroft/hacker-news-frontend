71ea9a57da9ede73f9e5fc6e7858af9e4c9c2e6a
Cori <coribeecroft@gmail.com>
2023-04-08 13:31:03 -0700

    In HN Fish, made it so that fish rotate slightly to match their y axis movement. This is currently only working for fish moving left.

--- src/HackerNewsFish.js
+++ src/HackerNewsFish.js
@@ -30,7 +30,7 @@ export function HackerNews() {
 
     useEffect(() => {
         if(storyIds.length == 0) { return; }
-        Promise.all(storyIds.slice(0, 150).map(id =>
+        Promise.all(storyIds.slice(0, 10).map(id =>
 			fetch(HN_API_URL + "/item/" + id + ".json")
 				.then(response => response.json())
 		)).then(stories => {
@@ -45,29 +45,67 @@ export function HackerNews() {
     function generateTargetYPosition(fishElement, initialYPosition) {
         return Math.min(
             Math.max(
-                (initialYPosition + getRandomSign()*(getRandomInt(0, 75) + 1)), 0
+                (initialYPosition + getRandomSign()*(getRandomInt(99, 200) + 1)), 0
             ),
             window.innerHeight - fishElement.getBoundingClientRect().height
         )
+    }
+
+    function preventOverRotation(f, newRotation) {
+        return f.rotationDirection === -1 ?
+            Math.max(newRotation, f.targetRotation) :
+            Math.min(newRotation, f.targetRotation)
+    }
+
+    function easeInOutCubic(t) {
+        return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+    }
+
+    function shortAngleDist(a0,a1) {
+        var max = Math.PI*2;
+        var da = (a1 - a0) % max;
+        return 2*da % max - da;
+    }
+
+    function getRotationAtTime(f, time) {
+        const totalTimeToAnimate = shortAngleDist(f.initialRotation, f.targetRotation)/(f.rotationDirection*f.rotationSpeed)
+        const timeElapsed = time - f.yStartTime
+        const animationProgress = totalTimeToAnimate == 0 ? 1 : Math.min(1, timeElapsed/totalTimeToAnimate);
+        const newRotation = f.initialRotation + easeInOutCubic(animationProgress)*shortAngleDist(f.initialRotation, f.targetRotation)
+
+        // TODO: handle this properly
+        // if(Number.isNaN(newRotation)) {
+        //     console.log(f.initialRotation, f.rotationDirection, f.rotationSpeed, timeElapsed);
+        // }
+
+        return preventOverRotation(f, newRotation);
     }
 
     function targetYPositionReached(f, newYPosition) {
         return (f.yDirection < 0 && newYPosition < f.targetYPosition) ||
             (f.yDirection > 0 && newYPosition > f.targetYPosition)
     }
+
     function targetXPositionReached(f) {
         const xPosition = f.initialXPosition + f.xDirection*f.xSpeed*(prevTimeRef.current - f.xStartTime);
         return (f.xDirection < 0 && xPosition < f.targetXPosition) ||
             (f.xDirection > 0 && xPosition > f.targetXPosition)
     }
 
+    function calculateTargetRotation(f, yDirection) {
+        return Math.atan(f.ySpeed/f.xSpeed)/6 * yDirection*-1
+    }
     function initializeFish(f, time) {
         const fishElement = f.ref.current;
         const initialXPosition = f.xDirection < 0 ? window.innerWidth : -1*fishElement.getBoundingClientRect().width;
         const initialYPosition = getRandomInt(0, (window.innerHeight - fishElement.getBoundingClientRect().height))
         fishElement.style.transform = `translate(${ initialXPosition }px, ${ initialYPosition }px)`
+
         const targetYPosition = generateTargetYPosition(fishElement, initialYPosition)
         const yDirection = (targetYPosition - initialYPosition)/Math.abs(targetYPosition - initialYPosition)
+        const initialRotation = 0;
+        const targetRotation = calculateTargetRotation(f, yDirection);
+        const rotationDirection = (targetRotation - initialRotation)/Math.abs((targetRotation - initialRotation));
         setFish(oldFish => oldFish.map(of => of.id === f.id ? {
                 ...f,
                 targetXPosition: f.xDirection < 0 ? -1*fishElement.getBoundingClientRect().width : window.innerWidth,
@@ -76,7 +114,10 @@ export function HackerNews() {
                 yStartTime: time,
                 yDirection,
                 initialXPosition,
-                initialYPosition
+                initialYPosition, 
+                initialRotation,
+                targetRotation,
+                rotationDirection,
             } : of)
         )
     }
@@ -84,22 +125,36 @@ export function HackerNews() {
     function updateFishPosition(f, time) {
         const newXPosition = f.initialXPosition + f.xDirection*f.xSpeed*(time - f.xStartTime)
         const newYPosition = f.initialYPosition + f.yDirection*f.ySpeed*(time - f.yStartTime)
-        f.ref.current.style.transform = `translate(${ newXPosition }px, ${ newYPosition }px)`
+        const newRotation = getRotationAtTime(f, time);
+        f.ref.current.style.transform = `
+            translate(${ newXPosition }px, ${ newYPosition }px)
+            rotate(${ newRotation }rad)`
 
         if(targetXPositionReached(f)) {
             setFish(oldFish => oldFish.filter(of => f.id !== of.id))
         } else if(targetYPositionReached(f, newYPosition)) {
             setFish(oldFish => oldFish.map(of => {
                 if(of.id !== f.id) { return of; }
+
                 const initialYPosition = newYPosition;
                 const targetYPosition = generateTargetYPosition(f.ref.current, initialYPosition);
-                const yDirection = (targetYPosition - initialYPosition)/Math.abs(targetYPosition - initialYPosition)
+                const yDirection = (targetYPosition - initialYPosition) /
+                    Math.abs(targetYPosition - initialYPosition)
+                const targetRotation = calculateTargetRotation(f, yDirection);
+                const initialRotation = newRotation;
+
                 return {
                     ...of,
                     initialYPosition,
                     targetYPosition,
                     yDirection,
-                    yStartTime: time
+                    yStartTime: time,
+                    initialRotation,
+                    rotationDirection: targetRotation === initialRotation ?
+                        f.rotationDirection
+                        : (targetRotation - initialRotation) /
+                            Math.abs(targetRotation - initialRotation),
+                    targetRotation
                 }
             }))
         }
@@ -134,7 +189,8 @@ export function HackerNews() {
                 const fishToAdd = generateFish(storyQueue.current.shift())
                 setFish(oldFish => oldFish.concat([ fishToAdd ]))
             }
-        }, 6000)
+        }, 4000)
+        // }, 6000)
         return () => clearInterval(interval)
     }, [])
 
@@ -142,8 +198,9 @@ export function HackerNews() {
         return {
             id: storyInfo.id,   // TODO: consider making this a different id
             targetXPosition: null, targetYPosition: null,
-            xDirection: getRandomSign(), xSpeed: 0.03,
-            yDirection: getRandomSign(), ySpeed: 0.03,
+            xDirection: -1/* getRandomSign() */, xSpeed: getRandomInt(2, 5)/100,
+            yDirection: getRandomSign(), ySpeed: getRandomInt(2, 5)/100,
+            rotationSpeed: Math.PI/(3000),
             storyInfo,
         }
     }
