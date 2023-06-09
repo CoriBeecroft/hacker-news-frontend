70b2a976a7d2a265c2921a4cce3f0796fe16797e
Cori <coribeecroft@gmail.com>
2023-06-09 12:53:01 -0700

    Did some work on the fish log.

--- src/hn-fish/FishLog.js
+++ src/hn-fish/FishLog.js
@@ -1,25 +1,21 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./FishLog.scss";
 
 
 export function FishLog(props) {
-    const ref = useRef()
-    let style = {};
-    if(!props.showFishLog) {
-        style = { transform: `translate(${ window.innerWidth }px, 0px)` }
-    } else if(ref.current) {
-        const xPosition = window.innerWidth - ref.current.getBoundingClientRect().width;
-        style = { transform: `translate(${ xPosition }px, 0px)` }
-    }
-
-    return <div ref={ ref } className="fish-log" style={ style }>
+    return <div { ...{
+        className: `fish-log ${props.showFishLog ? "" : "hidden" }`
+    }}>
         { props.addedStories.map(s => <FishLogItem key={ s.id } { ...s } />) }
     </div>
 }
 
 function FishLogItem(props) {
-    return <div className="fish-log-item">
+    const [ isNew, setIsNew ] = useState(true);
+    useEffect(() => setIsNew(false), [])
+
+    return <div className={ ["fish-log-item", isNew ? "new" : ""].join(" ") }>
         { `${ props.index + 1 }. ${ props.title }` }
     </div>
 }
