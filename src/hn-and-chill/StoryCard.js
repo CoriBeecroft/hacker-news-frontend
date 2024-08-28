4ca21497823af7b4404cfda7c8586eb3dbe590ad
Cori <coribeecroft@gmail.com>
2024-08-28 11:43:39 -0700

    Modified the fully expanded view in HN and Chill to display the data visualization gradient as the background of the whole expanded card, not just the StorySummary portion.

--- src/hn-and-chill/StoryCard.js
+++ src/hn-and-chill/StoryCard.js
@@ -81,13 +81,15 @@ function FullyExpandedStoryCard({ story, index, setState }) {
                 onClick: () => setState(COLLAPSED),
             }}
         >
-            <div className="story-card fully-expanded">
+            <div
+                className="story-card fully-expanded"
+                style={createGradientBackground({ ...story, large: true })}
+            >
                 <StorySummary
                     {...{
                         storyInfo: story,
                         index,
                         excludeNumber: true,
-                        style: createGradientBackground(story),
                     }}
                 />
                 <StoryContent
@@ -187,30 +189,55 @@ function StoryDetails({ score, time, descendants, type, by, url }) {
     )
 }
 
-const createGradientBackground = ({ score = 0, time, descendants = 0 }) => {
+const createGradientBackground = ({
+    score = 0,
+    time,
+    descendants = 0,
+    large = false,
+}) => {
     const mappedTime =
         255 - ((new Date().getTime() / 1000 - time) / (60 * 60 * 24 * 3)) * 255
     const mappedScore = (score / 500) * 180 + 75
     const mappedDescendants = descendants % 255
-    return {
-        backgroundImage: `radial-gradient(
-            ellipse at -20% 100%,
-            rgba(0, 0, ${mappedScore}, 0.25),
-            transparent 80%
-        ),
-        linear-gradient(
-            184deg,
-            rgba(${mappedTime}, 6, 15, 0.4),
-            rgba(${mappedTime}, 6, 15, 0.2) 10%,
-            rgba(0, 0, 0, 0) 25%
-        ),
-        radial-gradient(
-            ellipse at 120% 100%,
-            rgba(${mappedDescendants}, 0, ${mappedDescendants}, 0.25
-        ),
-            transparent 80%
-        )`,
-    }
+    return large
+        ? {
+              backgroundImage: `radial-gradient(
+                    ellipse at -20% 100%,
+                    rgba(0, 0, ${mappedScore}, 0.25),
+                    transparent ${0.6 * 80}%
+                ),
+                linear-gradient(
+                    184deg,
+                    rgba(${mappedTime}, 6, 15, 0.4),
+                    rgba(${mappedTime}, 6, 15, 0.2) ${1 * 10}%,
+                    rgba(0, 0, 0, 0) ${1 * 25}%
+                ),
+                radial-gradient(
+                    ellipse at 120% 100%,
+                    rgba(${mappedDescendants}, 0, ${mappedDescendants}, 0.25
+                ),
+                    transparent ${0.6 * 80}%
+                )`,
+          }
+        : {
+              backgroundImage: `radial-gradient(
+                    ellipse at -20% 100%,
+                    rgba(0, 0, ${mappedScore}, 0.25),
+                    transparent 80%
+                ),
+                linear-gradient(
+                    184deg,
+                    rgba(${mappedTime}, 6, 15, 0.4),
+                    rgba(${mappedTime}, 6, 15, 0.2) 10%,
+                    rgba(0, 0, 0, 0) 25%
+                ),
+                radial-gradient(
+                    ellipse at 120% 100%,
+                    rgba(${mappedDescendants}, 0, ${mappedDescendants}, 0.25
+                ),
+                    transparent 80%
+                )`,
+          }
 }
 
 const calculateStoryCardStyle = (expanded = false, storyCardPositionRef) => {
