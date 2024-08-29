4cba87e588c46da6c0e5e93c13ee172e39235059
Cori <coribeecroft@gmail.com>
2024-08-28 17:13:03 -0700

    Added an animation for the fully expanded story card entering and exiting. Fixed a couple of minor bugs in StoryCard.

--- src/hn-and-chill/StoryCard.js
+++ src/hn-and-chill/StoryCard.js
@@ -74,22 +74,63 @@ export function StoryCard({ index, story }) {
 }
 
 function FullyExpandedStoryCard({ story, index, setState }) {
+    const modalRef = useRef()
+    const storyCardRef = useRef()
+
+    useLayoutEffect(() => {
+        if (modalRef.current && storyCardRef.current) {
+            animate(modalRef.current, [{ opacity: 0 }, { opacity: 1 }], {
+                duration: 300,
+                easing: "ease",
+                fill: "forwards",
+            })
+
+            animate(
+                storyCardRef.current,
+                [{ transform: "scale(0.7)" }, { transform: "scale(1)" }],
+                { duration: 300, easing: "ease", fill: "forwards" }
+            )
+        }
+    }, [])
+
+    const collapseAndUnmount = useCallback(() => {
+        if (modalRef.current && storyCardRef.current) {
+            Promise.all([
+                animate(
+                    storyCardRef.current,
+                    [{ transform: "scale(1)" }, { transform: "scale(0.7)" }],
+                    { duration: 300, easing: "ease", fill: "forwards" }
+                ),
+                animate(modalRef.current, [{ opacity: 1 }, { opacity: 0 }], {
+                    duration: 300,
+                    easing: "ease",
+                    fill: "forwards",
+                }),
+            ]).then(() => setState(COLLAPSED))
+        }
+    }, [])
+
     return (
         <div
             {...{
+                ref: modalRef,
                 className: "modal-background",
-                onClick: () => setState(COLLAPSED),
+                onClick: collapseAndUnmount,
             }}
         >
             <div
-                className="story-card fully-expanded"
-                style={createGradientBackground({ ...story, large: true })}
+                {...{
+                    ref: storyCardRef,
+                    className: "story-card fully-expanded",
+                    style: createGradientBackground({ ...story, large: true }),
+                }}
             >
                 <StorySummary
                     {...{
                         storyInfo: story,
                         index,
                         excludeNumber: true,
+                        onClick: e => e.stopPropagation(),
                     }}
                 />
                 <StoryContent
@@ -130,6 +171,7 @@ function ExpandedStoryCard({
             animate(ref.current, [{ opacity: 0 }, { opacity: 1 }], {
                 duration: 100,
                 easing: "ease",
+                fill: "forwards",
             })
 
             animate(
@@ -138,7 +180,7 @@ function ExpandedStoryCard({
                     { transform: scaledTransform },
                     { transform: unscaledTransform },
                 ],
-                { duration: 300, easing: "ease" }
+                { duration: 300, easing: "ease", fill: "forwards" }
             )
         }
     }, [])
